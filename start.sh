08d5c998a493559a1e18b3c07e5aec49c0f6e200
Aschie4589 <tommaso.aschieri@gmail.com>
2025-03-14 15:35:57 +0100

    First Rpi commit

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
+# Detect docker
+if ! [ -x "$(command -v docker)" ]; then
+    echo "Docker is not installed. Please install Docker before running this script."
+    exit 1
+fi
+
+# Detect docker-compose
+if ! [ -x "$(command -v docker-compose)" ]; then
+    echo "Docker Compose is not installed. Please install Docker Compose before running this script."
+    exit 1
+fi
+
 # Setup swarming
 docker swarm init
 
