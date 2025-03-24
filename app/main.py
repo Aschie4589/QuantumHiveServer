c0e5ba63b273637b665240501ff03c0219b9c63b
Aschie4589 <tommaso.aschieri@gmail.com>
2025-03-24 19:16:42 +0100

    simple logging upload

--- app/main.py
+++ app/main.py
@@ -36,7 +36,6 @@ app = FastAPI(title="QuantumHiveAPI", lifespan=lifespan)
 
 
 
-
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(jobs.router, prefix="/jobs", tags=["jobs"])
